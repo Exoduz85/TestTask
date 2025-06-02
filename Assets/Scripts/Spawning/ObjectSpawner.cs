1d533745d84cbe68a87ae8b7a03d2d0bb6d97dae
Fredrik Wallander <f.m.wallander@gmail.com>
2025-06-02 17:12:57 +0200

    Add coloring by facing for object A in spawner
    Update scene gameobject references

--- Assets/Scripts/Spawning/ObjectSpawner.cs
+++ Assets/Scripts/Spawning/ObjectSpawner.cs
@@ -1,4 +1,5 @@
 ﻿using Animations;
+using Coloring;
 using Meshes;
 using Transformation;
 using UnityEngine;
@@ -8,6 +9,7 @@ namespace Spawning
 	public class ObjectSpawner : MonoBehaviour
 	{
 		public ComputeShader MeshCompute;
+		public ComputeShader ColorCompute;
 		public GameObject ObjectA;
 		public GameObject ObjectB;
 		public int SphereResolution = 64;
@@ -16,18 +18,23 @@ namespace Spawning
 		public float ConeBaseRadius = 0.5f;
 		public float ConeLength = 0.5f;
 
+		const string colorShaderName = "Unlit/Color";
+		const string vertexColorShaderName = "Unlit/VertexColor";
+		
 		void Awake()
 		{
-			ObjectA = CreateObject("Object A", new(-10, 0, 0), Color.red, MeshType.SphereWithNose);
-			ObjectB = CreateObject("Object B", new(10, 0, 0), Color.green, MeshType.Sphere);
+			ObjectA = CreateObject("Object A", new(-10, 0, 0), Color.red, MeshType.SphereWithNose, vertexColorShaderName);
+			ObjectB = CreateObject("Object B", new(10, 0, 0), Color.green, MeshType.Sphere, colorShaderName);
 			
 			AddLissajousAnimation(ObjectA);
 			AddLissajousAnimation(ObjectB, true);
 			
 			AddRotateTowardsTarget(ObjectA, ObjectB);
+			
+			AddFacingColorUpdater(ObjectA, ObjectB);
 		}
 
-		GameObject CreateObject(string objectName, Vector3 startingPosition, Color objectColor, MeshType meshType)
+		GameObject CreateObject(string objectName, Vector3 startingPosition, Color objectColor, MeshType meshType, string shaderName)
 		{
 			var obj = new GameObject(objectName)
 			{
@@ -44,7 +51,7 @@ namespace Spawning
 				_ => null
 			};
 
-			meshRenderer.material = new (Shader.Find("Unlit/Color"))
+			meshRenderer.material = new (Shader.Find(shaderName))
 			{
 				color = objectColor
 			};
@@ -63,6 +70,13 @@ namespace Spawning
 		{
 			var rotateTowards = rotateObject.AddComponent<RotateTowardsTarget>();
 			rotateTowards.target = target.transform;
+		}
+		
+		void AddFacingColorUpdater(GameObject toBeColored, GameObject facingTarget)
+		{
+			var colorUpdater = toBeColored.AddComponent<FacingColorUpdater>();
+			colorUpdater.Target = facingTarget.transform;
+			colorUpdater.ColorCompute = ColorCompute;
 		}
 	}
 }
