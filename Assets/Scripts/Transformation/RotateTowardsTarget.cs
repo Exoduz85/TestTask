2635bc587783c3b5436119a39bef8b29171459a5
Fredrik Wallander <f.m.wallander@gmail.com>
2025-06-02 16:23:41 +0200

    Add simple rotate to target script

---
+++ Assets/Scripts/Transformation/RotateTowardsTarget.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Transformation
+{
+	public class RotateTowardsTarget : MonoBehaviour
+	{
+		public Transform target;
+		public float angularSpeed = 33f;
+
+		void Update()
+		{
+			if (target == null)
+				return;
+
+			var direction = (target.position - transform.position).normalized;
+
+			if (direction.sqrMagnitude < 0.0001f)
+				return;
+
+			var targetRotation = Quaternion.LookRotation(direction, Vector3.up);
+
+			transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, angularSpeed * Time.deltaTime);
+		}
+	}
+}
