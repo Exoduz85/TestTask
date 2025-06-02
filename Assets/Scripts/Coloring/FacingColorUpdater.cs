cd467ce89c938c40e7063cb8aedd36c7dd6bd8d2
Fredrik Wallander <f.m.wallander@gmail.com>
2025-06-02 17:11:37 +0200

    Add facing by color class for computing color of object

---
+++ Assets/Scripts/Coloring/FacingColorUpdater.cs
@@ -0,0 +1,63 @@
+﻿using Helper;
+using UnityEngine;
+
+namespace Coloring
+{
+	public class FacingColorUpdater : MonoBehaviour
+	{
+		public ComputeShader ColorCompute;
+		public Transform Target;
+		
+		MeshFilter meshFilter;
+		ComputeBuffer colorBuffer;
+		ComputeBuffer vertexBuffer;
+		Vector3[] vertices;
+		Color[] colors;
+		int computeKernel;
+		
+		const string kernelName = "CSColorByFacing";
+		const int maxThreads = 64;
+
+		void Start()
+		{
+			meshFilter = GetComponent<MeshFilter>();
+			var mesh = meshFilter.mesh;
+
+			vertices = mesh.vertices;
+			colors = new Color[vertices.Length];
+
+			vertexBuffer = new(vertices.Length, sizeof(float) * 3);
+			colorBuffer = new(vertices.Length, sizeof(float) * 4);
+
+			vertexBuffer.SetData(vertices);
+			colorBuffer.SetData(colors);
+
+			computeKernel = ColorCompute.FindKernel(kernelName);
+			ColorCompute.SetInt(ShaderPropIDs.ColorByFacing.VertexCountPropID, vertices.Length);
+			ColorCompute.SetBuffer(computeKernel, ShaderPropIDs.VerticesPropID, vertexBuffer);
+			ColorCompute.SetBuffer(computeKernel, ShaderPropIDs.ColorByFacing.ColorsPropID, colorBuffer);
+		}
+
+		void Update()
+		{
+			var toTarget = (Target.position - transform.position).normalized;
+			var forward = transform.forward;
+
+			ColorCompute.SetVector(ShaderPropIDs.ColorByFacing.ToTargetPropID, toTarget);
+			ColorCompute.SetVector(ShaderPropIDs.ColorByFacing.ObjectForwardPropID, forward);
+			
+			ColorCompute.Dispatch(computeKernel, Mathf.CeilToInt(vertices.Length / (float)maxThreads), 1, 1);
+
+			colorBuffer.GetData(colors);
+
+			var mesh = meshFilter.mesh;
+			mesh.colors = colors;
+		}
+
+		void OnDestroy()
+		{
+			vertexBuffer?.Release();
+			colorBuffer?.Release();
+		}
+	}
+}
